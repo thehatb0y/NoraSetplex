1d710cf75ae551a008841e80d013c59d23d27b2c
Mateus C <mts.c@live.com>
2022-08-23 22:49:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,14 @@ def main():
     #Customers that received credit, starts in 0
     received_credit = 0
 
+    #Customer Test
+    customerID =  30189254
     #Set the date to receive the credit 
     data= "2022-08-06T18:58"
     data = data[:-6]
 
     #Check if connection is working, if == 200 get the total pages
-    response = requests.get(header.set_url(page, streamname), headers=header.set_header(token, jsessionid, streamname))
+    response = requests.get(nora.set_url(page, streamname), headers=nora.set_header(token, jsessionid, streamname, 0, False, False, False))
     if response.status_code == 200:
         total_pages = response.json()['content']['totalPages']
     else:
@@ -34,19 +36,27 @@ def main():
         print(f"Scrapping page {page}")
         for contact in response.json()['content']['content']:
             if contact['expirationTime'] != None:
+                #if contact['id'] ==  customerID:
                 if contact['expirationTime'][:-6] == data:
-                    print("Customerid:"+contact['id'])  
-                    response = requests.request("POST", 
-                        header.set_url_subs(contact['id'], streamname), 
-                        headers = header.set_onedaypayload_header(token, jsessionid, contact['id'], streamname),
-                        data = header.set_onedaypayload(contact['id'])
+                    #r = requests.get(nora.set_checkCustomerUrl_subs(contact['id'], streamname), headers=nora.set_header(token, jsessionid, streamname, 0, False, False, True))
+                    #r = r.json()
+
+                    #requests.request("POST", 
+                    #    nora.set_subscribersUrl_subs(streamname), 
+                    #    headers = nora.set_header(token, jsessionid, streamname, contact['id'], False, True, False),
+                    #    data = nora.set_duplicateAccPayload(str(r['name'])+"3", str(111111), r['firstname'], r['lastname']+"3", r['email'], r['phone'], r['address'], r['city'], r['zipcode'], r['country'])
+                    #)
+                    #exit()
+                    requests.request("POST", 
+                        nora.set_url_subs(contact['id'], streamname), 
+                        headers = nora.set_header(token, jsessionid, streamname, contact['id'], False, False, False),
+                        data = nora.set_setSubPayload(contact['id'], 2)
                     )
                     received_credit += 1
-        response = requests.get(header.set_url(page, streamname), headers=header.set_header(token, jsessionid, streamname))
+        response = requests.get(nora.set_url(page, streamname), headers=nora.set_header(token, jsessionid, streamname, 0, False, False, False))
         page = response.json()['content']['number']
     time_end = time.time()
     print(f'Total time elapsed: {time_end - time_init}')
     print(f'Total Customer that received credit: {received_credit}')
     
 if __name__ == "__main__":
-    main()
