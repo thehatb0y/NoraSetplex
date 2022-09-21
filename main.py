2fa5605e12940bbc316eb0c9959220b57f8c718a
Mateus C <mts.c@live.com>
2022-09-21 10:31:26 -0300

    Update main.py
    Added new json export function

--- main.py
+++ main.py
@@ -21,20 +21,25 @@ def main():
         print("0 to exit")
         print("1 to add time to a customer")
         print("2 to duplicate a customer")
+        print("3 to export to Json")
 
         r = int(input())
-        if r == 2:
+        if r == 3:
+            print("Copying...")
+            nora.noraJsonExport(token, jsessionid, streamname, data, daysToGive)
+            print("Exporting Complete")
+        elif r == 2:
             print("Type Customer ID")
             customerID = int(input())
             nora.duplicate(streamname, token, jsessionid, customerID)
         elif r == 1:
             print("Type How Many Days To Give")
             daysToGive = int(input())
-            nora.add_time(streamname, token, jsessionid, data, daysToGive)
+            nora.noracheck(token, jsessionid, streamname, data, daysToGive)
         elif r == 0:
             print("Exiting...")
             exit()
         
-    
 if __name__ == "__main__":
     main()
+
